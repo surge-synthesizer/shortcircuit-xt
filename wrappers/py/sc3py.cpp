9cafefde3fe2ee7d5a365fbcd218b0a13f26434b
Paul <baconpaul@users.noreply.github.com>
2020-12-19 15:57:36 -0500

    The Python Wrapper actually creates an SC3 sampler! (#8)

--- wrappers/py/sc3py.cpp
+++ wrappers/py/sc3py.cpp
@@ -1,23 +1,36 @@
-#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 
-#include <thread>
+#include <string>
 #include <utility>
 
-#include "filesystem/import.h"
 #include "sampler.h"
 #include "version.h"
 
+// This is obviously gross
+void *hInstance = nullptr;
+
 namespace py = pybind11;
 
-int createSC3(float sampleRate) { return (int)sampleRate; }
+struct SC3PY : public sampler
+{
+    explicit SC3PY(int numOuts) : sampler(nullptr, numOuts) {}
+};
+
+std::unique_ptr<SC3PY> createSC3(float sampleRate, int nChannels)
+{
+    auto res = std::make_unique<SC3PY>(2);
+    res->set_samplerate(sampleRate);
+    return res;
+}
 
 PYBIND11_MODULE(shortcircuit3py, m)
 {
     m.doc() = "Python bindings for ShortCircuit3";
-    m.def("createSC3", &createSC3, "Create an SC3 instance", py::arg("sampleRate"));
+    m.def("createSC3", &createSC3, "Create an SC3 instance", py::arg("sampleRate"),
+          py::arg("nChannels") = py::int_(2));
     m.def(
         "getVersion", []() { return ShortCircuit::Build::FullVersionStr; },
         "Get the version of ShortCircuit");
+
+    py::class_<SC3PY>(m, "ShortCircuit3Sampler");
 }
