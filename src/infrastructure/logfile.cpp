24a19f98ae510dccaf4e9a811121ea1c17ffada7
Paul Walker <paul@pwjw.com>
2020-12-29 20:16:12 -0500

    Cleanup the Logfile API
    Closes #8

--- src/infrastructure/logfile.cpp
+++ src/infrastructure/logfile.cpp
@@ -1,52 +1,31 @@
-#include "logfile.h"
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
+/*
+** ShortCircuit3 is Free and Open Source Software
+**
+** ShortCircuit is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html; The authors of the code
+** reserve the right to re-license their contributions under the MIT license in the
+** future at the discretion of the project maintainers.
+**
+** Copyright 2004-2021 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** ShortCircuit was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made ShortCircuit
+** open source in December 2020.
+*/
 
+#include "infrastructure/logfile.h"
 #include <iostream>
 
 
-bool log_updated = false;
-char log_entry[256];
-
-void write_log(const char *text)
+namespace SC3::Log
 {
-    log_updated = true;
-    strcpy(log_entry, text);
-
-    std::cout << "[] " << text << std::endl;
-
-    // open file
-    /*FILE *f = fopen("c:\\shortcircuit.log","a+");
-
-    char tmpbuf[128];
-    _strtime( tmpbuf );
-
-    // write timestamp
-    fprintf(f,"[%s] ", tmpbuf);
-    // write message
-    fprintf(f,"%s\n",text);
-
-    //fflush(f);
-    //close file
-    //fclose(f);*/
+void write_log(const char* c)
+{
+    /*
+     * Obviously, improve this
+     */
+    std::cout << "SC3LOG: " << c << std::endl;
 }
-
-void write_log(int number)
-{
-    /*// open file
-    //f = fopen(filename,"a+");
-    FILE *f = fopen("c:\\shortcircuit.log","a+");
-
-    char tmpbuf[128];
-    _strtime( tmpbuf );
-
-    // write timestamp
-    fprintf(f,"[%s] ", tmpbuf);
-    // write message
-    fprintf(f,"%i\n",number);
-
-    //fflush(f);
-    //close file
-    fclose(f);*/
 }
