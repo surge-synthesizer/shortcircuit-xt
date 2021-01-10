74a1801ce65e64121d0babeb7687ee7308792766
Paul <baconpaul@users.noreply.github.com>
2021-01-10 14:42:07 -0500

    Finalize structure of log -> ui (#86)
    The mNotify thing was a bit temporary, although we had a FIFO queue
    already, but restructure it so (1) the processor is the log reciever
    not an ancilliary class, (2) the level comes through all the way to
    the editor idle as data not text, and (3) everything still works!

--- wrappers/juce/SC3Processor.h
+++ wrappers/juce/SC3Processor.h
@@ -11,26 +11,19 @@
 #include "sampler.h"
 #include <JuceHeader.h>
 
-class SC3ProcessorLogger;
-// temporary
-// the proper way to do this would probably be with a fifo between processor and editor
-class EditorNotify {
+class LogDisplayListener
+{
   public:
-    virtual void setLogText(const std::string &txt)=0;
+    virtual void handleLogMessage(SC3::Log::Level lev, const std::string &txt) = 0;
 };
-
 
 //==============================================================================
 /**
  */
-class SC3AudioProcessor : public juce::AudioProcessor
+class SC3AudioProcessor : public juce::AudioProcessor, public SC3::Log::LoggingCallback
 {
   
   public:
-  // editor will set and clear this...
-  // shitty i know, but it's temporary
-  EditorNotify *mNotify;
-
     //==============================================================================
     SC3AudioProcessor();
     ~SC3AudioProcessor() override;
@@ -38,6 +31,19 @@ class SC3AudioProcessor : public juce::AudioProcessor
     //==============================================================================
     void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
+
+    std::unordered_set<LogDisplayListener *> logDispList;
+    void addLogDisplayListener(LogDisplayListener *e) { logDispList.insert(e); }
+    void removeLogDisplayListener(LogDisplayListener *e) { logDispList.erase(e); }
+    SC3::Log::Level getLevel() override { return SC3::Log::Level::Debug; }
+
+    void message(SC3::Log::Level lev, const std::string &msg) override
+    {
+        for (auto l : logDispList)
+        {
+            l->handleLogMessage(lev, msg);
+        }
+    }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
     bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
@@ -68,7 +74,6 @@ class SC3AudioProcessor : public juce::AudioProcessor
     void getStateInformation(juce::MemoryBlock &destData) override;
     void setStateInformation(const void *data, int sizeInBytes) override;
 
-    std::unique_ptr<SC3ProcessorLogger>mLogger; // must destroy before sc3 because flush in logger needs its cb
     std::unique_ptr<sampler> sc3;
     
 
@@ -79,20 +84,3 @@ class SC3AudioProcessor : public juce::AudioProcessor
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SC3AudioProcessor)
 };
 
-class SC3ProcessorLogger : public SC3::Log::LoggingCallback {
-
-    SC3AudioProcessor *mParent;
-    SC3::Log::Level getLevel() override {
-        return SC3::Log::Level::Debug;
-    }
-    void message(SC3::Log::Level lev, const std::string &msg) override {
-        if(mParent->mNotify) {
-          std::string t = SC3::Log::getShortLevelStr(lev);
-          t += msg;
-          mParent->mNotify->setLogText(t);
-        }
-    }
-public:
-    SC3ProcessorLogger(SC3AudioProcessor *p) : mParent(p) {}
-    ~SC3ProcessorLogger() {}
-};
