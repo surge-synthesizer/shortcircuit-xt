9e3b2122d3362012d194eb470d8d16d7378e3f17
James Alan Nguyen <djtubig.malicex@gmail.com>
2021-02-08 01:04:19 +1100

    add license header.

--- src/vembertech/vt_dsp/basic_dsp.cpp
+++ src/vembertech/vt_dsp/basic_dsp.cpp
@@ -1,3 +1,20 @@
+/*
+** ShortCircuit3 is Free and Open Source Software
+**
+** ShortCircuit is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html; The authors of the code
+** reserve the right to re-license their contributions under the MIT license in the
+** future at the discretion of the project maintainers.
+**
+** Copyright 2004-2021 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** ShortCircuit was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made ShortCircuit
+** open source in December 2020.
+*/
+
 #include "basic_dsp.h"
 #include <cassert>
 
