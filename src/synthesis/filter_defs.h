ad1bd013d5c9babc7e3ee85cd4feef1fab8d67fd
haenkel <100365267+haenkel@users.noreply.github.com>
2022-04-12 14:44:00 +0200

    Add license headers (#277)
    Closes #153
    
    Co-authored-by: nunya <blah@blah.blah>

--- src/synthesis/filter_defs.h
+++ src/synthesis/filter_defs.h
@@ -1,3 +1,20 @@
+/*
+** Shortcircuit XT is Free and Open Source Software
+**
+** Shortcircuit is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html; The authors of the code
+** reserve the right to re-license their contributions under the MIT license in the
+** future at the discretion of the project maintainers.
+**
+** Copyright 2004-2022 by various individuals as described by the git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/shortcircuit-xt.git
+**
+** Shortcircuit was a commercial product from 2004-2018, with copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Shortcircuit
+** open source in December 2020.
+*/
+
 #pragma once
 #include "filter.h"
 #include "morphEQ.h"
@@ -1231,8 +1248,7 @@ class alignas(16) chorus : public filter
     lipol_ps feedback, width;
     __m128 voicepanL4 alignas(16)[4], voicepanR4 alignas(16)[4];
     biquadunit lp, hp;
-    float buffer alignas(
-        16)[max_delay_length + FIRipol_N];
+    float buffer alignas(16)[max_delay_length + FIRipol_N];
     // s� kan den interpoleras med SSE utan wrap
   public:
     chorus(float *, int *);
--- wrappers/juce/widgets/CompactVUMeter.h
+++ wrappers/juce/widgets/CompactVUMeter.h
@@ -1,6 +1,19 @@
-//
-// Created by Paul Walker on 1/6/22.
-//
+/*
+** Shortcircuit XT is Free and Open Source Software
+**
+** Shortcircuit is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html; The authors of the code
+** reserve the right to re-license their contributions under the MIT license in the
+** future at the discretion of the project maintainers.
+**
+** Copyright 2004-2022 by various individuals as described by the git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/shortcircuit-xt.git
+**
+** Shortcircuit was a commercial product from 2004-2018, with copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Shortcircuit
+** open source in December 2020.
+*/
 
 #ifndef SHORTCIRCUIT_COMPACTVUMETER_H
 #define SHORTCIRCUIT_COMPACTVUMETER_H
