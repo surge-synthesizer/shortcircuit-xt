0631df16e2de3475c5837ea412ae5155df4e5122
Paul Walker <paul@pwjw.com>
2023-02-09 12:22:54 -0500

    Automatic header in comment script running

---
+++ scripts/fix_file_comments.pl
@@ -0,0 +1,106 @@
+#!/usr/bin/perl
+
+
+use File::Find;
+use File::Basename;
+
+find(
+    {
+        wanted => \&findfiles,
+    },
+    'src-next'
+);
+
+
+find(
+    {
+        wanted => \&findfiles,
+    },
+    'ui-next'
+);
+
+
+sub findfiles
+{
+
+    $header = <<EOH;
+/*
+ * ShortCircuit XT - a Surge Synth Team product
+ *
+ * A fully featured creative sampler, available as a standalone
+ * and plugin for multiple platforms.
+ *
+ * Copyright 2019 - 2023, Various authors, as described in the github
+ * transaction log.
+ *
+ * ShortCircuitXT is released under the Gnu General Public Licence
+ * V3 or later (GPL-3.0-or-later). The license is found in the file
+ * "LICENSE" in the root of this repository or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Individual sections of code which comprises ShortCircuitXT in this
+ * repository may also be used under an MIT license. Please see the
+ * section  "Licensing" in "README.md" for details.
+ *
+ * ShortCircuitXT is inspired by, and shares some code with, the
+ * commercial product ShortCircuit 1 and 2, released by VemberTech
+ * in the mid 2000s. The code for ShortCircuit 2 was opensourced in
+ * 2020 at the outset of this project.
+ *
+ * All source for ShortCircuitXT is available at
+ * https://github.com/surge-synthesizer/shortcircuit-xt
+ */
+EOH
+
+    $f = $File::Find::name;
+    if ($f =~ m/\.h$/ or $f =~ m/.cpp$/)
+    {
+        #To search the files inside the directories
+        print "Processing $f\n";
+
+        $q = basename($f);
+        print "$q\n";
+        open(IN, "<$q") || die "Cant open IN $!";
+        open(OUT, "> ${q}.bak") || die "Cant open BAK $!";
+
+        $nonBlank = 0;
+        $inComment = 0;
+        while(<IN>)
+        {
+            if ($nonBlank)
+            {
+                print OUT
+            }
+            else
+            {
+                if (m:^\s*/\*:) {
+                    $inComment = 1;
+                }
+                elsif (m:\s*\*/:)
+                {
+                    print OUT $header;
+                    $nonBlank = true;
+                    $inComment = false;
+                }
+                elsif ($inComment)
+                {
+
+                }
+                elsif (m:^//:)
+                {
+
+                }
+                else
+                {
+                    print OUT $header;
+                    $nonBlank = true;
+                    print OUT;
+
+                }
+            }
+        }
+        close(IN);
+        close(OUT);
+        system("mv ${q}.bak ${q}");
+    }
+}
