d353277883e086faa094dfa703724f314d514f21
Paul Walker <paul@pwjw.com>
2023-06-08 10:10:10 -0400

    Some minor UI tweaks
    Mostly light skin working and code structured so it is more obvious
    how to do that.

--- src-ui/connectors/SCXTStyleSheetCreator.cpp
+++ src-ui/connectors/SCXTStyleSheetCreator.cpp
@@ -41,29 +41,9 @@ namespace scxt::ui::connectors
 {
 using sheet_t = sst::jucegui::style::StyleSheet;
 namespace comp = sst::jucegui::components;
-const sheet_t::ptr_t
-SCXTStyleSheetCreator::setup(sst::jucegui::style::StyleSheet::BuiltInTypes baseType)
+
+void SCXTStyleSheetCreator::makeDarkColors(const sheet_t::ptr_t &base)
 {
-    widgets::Tooltip::Styles::initialize();
-
-    sheet_t::addClass(SCXTStyleSheetCreator::ModulationEditorVSlider)
-        .withBaseClass(comp::VSlider::Styles::styleClass);
-    sheet_t::addClass(SCXTStyleSheetCreator::ModulationEditorKnob)
-        .withBaseClass(comp::Knob::Styles::styleClass);
-    sheet_t::addClass(SCXTStyleSheetCreator::ModulationTabs)
-        .withBaseClass(comp::NamedPanel::Styles::styleClass);
-    sheet_t::addClass(SCXTStyleSheetCreator::ModulationMatrixToggle)
-        .withBaseClass(comp::ToggleButton::Styles::styleClass);
-    sheet_t::addClass(SCXTStyleSheetCreator::ModulationMatrixMenu)
-        .withBaseClass(SCXTStyleSheetCreator::ModulationMatrixToggle);
-    sheet_t::addClass(SCXTStyleSheetCreator::InformationLabel)
-        .withBaseClass(comp::ControlStyles::styleClass);
-
-    const auto &base = sheet_t::getBuiltInStyleSheet(baseType);
-    base->setColour(widgets::Tooltip::Styles::styleClass, widgets::Tooltip::Styles::bordercol,
-                    base->getColour(sst::jucegui::components::NamedPanel::Styles::styleClass,
-                                    sst::jucegui::components::NamedPanel::Styles::labelrulecol));
-
     base->setColour(ModulationEditorVSlider, comp::VSlider::Styles::guttercol,
                     juce::Colour(0x39, 0x39, 0x39));
     base->setColour(ModulationEditorVSlider, comp::VSlider::Styles::gutterhovcol,
@@ -151,6 +131,131 @@ SCXTStyleSheetCreator::setup(sst::jucegui::style::StyleSheet::BuiltInTypes baseT
 
     base->setColour(InformationLabel, comp::ControlStyles::controlLabelCol,
                     juce::Colour(0x88, 0x88, 0x88));
+}
+
+void SCXTStyleSheetCreator::makeLightColors(const sheet_t::ptr_t &base)
+{
+    base->setColour(ModulationEditorVSlider, comp::VSlider::Styles::guttercol,
+                    juce::Colour(200, 200, 210));
+    base->setColour(ModulationEditorVSlider, comp::VSlider::Styles::gutterhovcol,
+                    juce::Colour(220, 220, 240));
+    base->setColour(ModulationEditorVSlider, comp::VSlider::Styles::valcol,
+                    juce::Colour(0x27, 0x88, 0xD6));
+    base->setColour(ModulationEditorVSlider, comp::VSlider::Styles::handlecol,
+                    juce::Colour(0xC4, 0xC4, 0xC4));
+
+    base->setColour(ModulationTabs, comp::NamedPanel::Styles::selectedtabcol,
+                    juce::Colour(0x27, 0x88, 0xD6));
+
+    base->setColour(ModulationEditorKnob, comp::Knob::Styles::guttercol,
+                    juce::Colour(200, 200, 210));
+    base->setColour(ModulationEditorKnob, comp::Knob::Styles::gutterhovcol,
+                    juce::Colour(220, 220, 240));
+    base->setColour(ModulationEditorKnob, comp::Knob::Styles::valcol,
+                    juce::Colour(0x27, 0x88, 0xD6));
+    base->setColour(ModulationEditorKnob, comp::Knob::Styles::handlecol,
+                    juce::Colour(0xC4, 0xC4, 0xC4));
+    base->setColour(ModulationEditorKnob, comp::Knob::Styles::gradientcenter,
+                    juce::Colour(0x27, 0x68, 0xA6));
+
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::onbgcol,
+                    juce::Colour(220, 220, 220));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::hoveronbgcol,
+                    juce::Colour(220, 220, 240));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::offbgcol,
+                    juce::Colour(200, 200, 210));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::hoveroffbgcol,
+                    juce::Colour(200, 200, 210));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::textoncol,
+                    juce::Colour(0x00, 0x00, 0x40));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::textoffcol,
+                    juce::Colour(0x88, 0x88, 0x88));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::borderoncol,
+                    juce::Colour(0xFF, 0x90, 0x00));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::bordercol,
+                    juce::Colour(0x35, 0x35, 0x35));
+    base->setColour(comp::MenuButton::Styles::styleClass, comp::MenuButton::Styles::texthoveroncol,
+                    juce::Colour(0x30, 0x30, 0x90));
+
+    base->setColour(comp::ToggleButton::Styles::styleClass, comp::ToggleButton::Styles::onbgcol,
+                    juce::Colour(200, 200, 210));
+    base->setColour(comp::ToggleButton::Styles::styleClass,
+                    comp::ToggleButton::Styles::hoveronbgcol, juce::Colour(220, 220, 240));
+    base->setColour(comp::ToggleButton::Styles::styleClass, comp::ToggleButton::Styles::offbgcol,
+                    juce::Colour(0xA0, 0xA0, 0xA0));
+    base->setColour(comp::ToggleButton::Styles::styleClass,
+                    comp::ToggleButton::Styles::hoveroffbgcol, juce::Colour(0x15, 0x15, 0x15));
+    base->setColour(comp::ToggleButton::Styles::styleClass, comp::ToggleButton::Styles::textoncol,
+                    juce::Colour(0x00, 0x00, 0x40));
+    base->setColour(comp::ToggleButton::Styles::styleClass, comp::ToggleButton::Styles::textoffcol,
+                    juce::Colour(0x40, 0x40, 0x40));
+    base->setColour(comp::ToggleButton::Styles::styleClass, comp::ToggleButton::Styles::borderoncol,
+                    juce::Colour(0xFF, 0x90, 0x00));
+    base->setColour(comp::ToggleButton::Styles::styleClass, comp::ToggleButton::Styles::bordercol,
+                    juce::Colour(0x35, 0x35, 0x35));
+    base->setColour(comp::ToggleButton::Styles::styleClass,
+                    comp::ToggleButton::Styles::texthoveroncol, juce::Colour(0x30, 0x30, 0x50));
+
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::onbgcol,
+                    juce::Colour(200, 200, 210));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::hoveronbgcol,
+                    juce::Colour(220, 220, 240));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::offbgcol,
+                    juce::Colour(0xA0, 0xA0, 0xA0));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::hoveroffbgcol,
+                    juce::Colour(0xA0, 0xA0, 0xC0));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::textoncol,
+                    juce::Colour(0x00, 0x00, 0x40));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::textoffcol,
+                    juce::Colour(0x30, 0x30, 0x30));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::borderoncol,
+                    juce::Colour(0x27, 0x88, 0xD6));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::bordercol,
+                    juce::Colour(0x15, 0x15, 0x15));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::texthoveroncol,
+                    juce::Colour(0x00, 0x00, 0x40));
+
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::textoncol,
+                    juce::Colour(0x00, 0x00, 0x00));
+    base->setColour(ModulationMatrixToggle, comp::ToggleButton::Styles::texthoveroncol,
+                    juce::Colour(0x00, 0x00, 0x40));
+
+    base->setColour(InformationLabel, comp::ControlStyles::controlLabelCol,
+                    juce::Colour(0x88, 0x88, 0x88));
+}
+
+const sheet_t::ptr_t
+SCXTStyleSheetCreator::setup(sst::jucegui::style::StyleSheet::BuiltInTypes baseType)
+{
+    widgets::Tooltip::Styles::initialize();
+
+    sheet_t::addClass(SCXTStyleSheetCreator::ModulationEditorVSlider)
+        .withBaseClass(comp::VSlider::Styles::styleClass);
+    sheet_t::addClass(SCXTStyleSheetCreator::ModulationEditorKnob)
+        .withBaseClass(comp::Knob::Styles::styleClass);
+    sheet_t::addClass(SCXTStyleSheetCreator::ModulationTabs)
+        .withBaseClass(comp::NamedPanel::Styles::styleClass);
+    sheet_t::addClass(SCXTStyleSheetCreator::ModulationMatrixToggle)
+        .withBaseClass(comp::ToggleButton::Styles::styleClass);
+    sheet_t::addClass(SCXTStyleSheetCreator::ModulationMatrixMenu)
+        .withBaseClass(SCXTStyleSheetCreator::ModulationMatrixToggle);
+    sheet_t::addClass(SCXTStyleSheetCreator::InformationLabel)
+        .withBaseClass(comp::ControlStyles::styleClass);
+
+    const auto &base = sheet_t::getBuiltInStyleSheet(baseType);
+
+    if (baseType == sst::jucegui::style::StyleSheet::DARK)
+    {
+        makeDarkColors(base);
+    }
+    else
+    {
+        makeLightColors(base);
+    }
+
+    base->setColour(widgets::Tooltip::Styles::styleClass, widgets::Tooltip::Styles::bordercol,
+                    base->getColour(sst::jucegui::components::NamedPanel::Styles::styleClass,
+                                    sst::jucegui::components::NamedPanel::Styles::labelrulecol));
 
     base->setFont(comp::NamedPanel::Styles::styleClass, comp::NamedPanel::Styles::regionLabelFont,
                   juce::Font(11));
