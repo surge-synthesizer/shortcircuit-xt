fc1aa87b34ec555f38d97171cc3a35663eff528e
Paul Walker <paul@pwjw.com>
2024-04-08 19:31:39 -0400

    Presonus/S1 uses a 'layer' spec in multisapmle. Use
    The spec says this isn't how its supposed to be! But seems it is.

--- src/sample/multisample_support/multisample_import.cpp
+++ src/sample/multisample_support/multisample_import.cpp
@@ -34,6 +34,9 @@ bool importMultisample(const fs::path &p, engine::Engine &engine)
         fileToIndex[file_stat.m_filename] = i;
     }
 
+    SCLOG("Read multisample '" << p.filename().u8string() << "' with "
+                               << (int)mz_zip_reader_get_num_files(&zip_archive) << " components");
+
     // Step two grab the multisample.xml
     if (fileToIndex.find("multisample.xml") == fileToIndex.end())
     {
@@ -69,6 +72,78 @@ bool importMultisample(const fs::path &p, engine::Engine &engine)
         return false;
     }
 
+    auto addSampleFromElement = [&p, &part, &engine, &zip_archive, &fileToIndex,
+                                 &addedGroupIndices](TiXmlElement *fc, int32_t group_index = -1) {
+        /*
+         * <sample file="60 Clavinet E5 05.wav" gain="-0.96" group="4" parameter-1="0.0000"
+    parameter-2="0.0000" parameter-3="0.0000" reverse="false" sample-start="0.000"
+    sample-stop="317919.000" zone-logic="always-play"> <key high="96" low="88" root="88"
+    track="1.0000" tune="0.00"/> <velocity low="125"/> <select/> <loop fade="0.0000" mode="off"
+    start="0.000" stop="317919.000"/>
+    </sample>
+         */
+        if (!fc->Attribute("file"))
+        {
+            SCLOG("Sample is not a file");
+            return false;
+        }
+
+        size_t ssize{0};
+        auto data = mz_zip_reader_extract_to_heap(&zip_archive, fileToIndex[fc->Attribute("file")],
+                                                  &ssize, 0);
+
+        auto lsid = engine.getSampleManager()->setupSampleFromMultifile(
+            p, fileToIndex[fc->Attribute("file")], data, ssize);
+        free(data);
+
+        if (!lsid.has_value())
+        {
+            SCLOG("Wierd - no lsid value");
+            return false;
+        }
+
+        auto kr{90}, ks{0}, ke{127}, vs{0}, ve{127};
+        auto key = fc->FirstChildElement("key");
+        auto vel = fc->FirstChildElement("velocity");
+        if (key)
+        {
+            key->QueryIntAttribute("root", &kr);
+            key->QueryIntAttribute("low", &ks);
+            key->QueryIntAttribute("high", &ke);
+        }
+        if (vel)
+        {
+            vel->QueryIntAttribute("low", &vs);
+            vel->QueryIntAttribute("high", &ve);
+        }
+
+        auto group_id = 0;
+        if (group_index == -1)
+        {
+            auto group{0};
+            fc->QueryIntAttribute("group", &group);
+
+            if (group < 0 || group > addedGroupIndices.size())
+            {
+                SCLOG("Bad group : " << group);
+                return false;
+            }
+            group_id = addedGroupIndices[group];
+        }
+
+        auto &g = part->getGroup(group_id);
+        auto z = std::make_unique<engine::Zone>(*lsid);
+        z->mapping.rootKey = kr;
+        z->mapping.keyboardRange.keyStart = ks;
+        z->mapping.keyboardRange.keyEnd = ke;
+        z->mapping.velocityRange.velStart = vs;
+        z->mapping.velocityRange.velEnd = ve;
+        z->attachToSample(*engine.getSampleManager());
+        g->addZone(z);
+
+        return true;
+    };
+
     auto fc = rt->FirstChildElement();
     std::string name{};
     if (rt->Attribute("name"))
@@ -92,76 +167,50 @@ bool importMultisample(const fs::path &p, engine::Engine &engine)
         }
         else if (eln == "sample")
         {
-            /*
-             * <sample file="60 Clavinet E5 05.wav" gain="-0.96" group="4" parameter-1="0.0000"
-parameter-2="0.0000" parameter-3="0.0000" reverse="false" sample-start="0.000"
-sample-stop="317919.000" zone-logic="always-play"> <key high="96" low="88" root="88" track="1.0000"
-tune="0.00"/> <velocity low="125"/> <select/> <loop fade="0.0000" mode="off" start="0.000"
-stop="317919.000"/>
-</sample>
-             */
-            if (!fc->Attribute("file"))
+            if (!addSampleFromElement(fc))
             {
-                SCLOG("Sample is not a file");
                 return false;
             }
+        }
+        else if (eln == "layer")
+        {
+            // Sigh. Presonus does something a little different
+            auto groupId = part->addGroup() - 1;
+            auto &group = part->getGroup(groupId);
 
-            size_t ssize{0};
-            auto data = mz_zip_reader_extract_to_heap(
-                &zip_archive, fileToIndex[fc->Attribute("file")], &ssize, 0);
-
-            auto lsid = engine.getSampleManager()->setupSampleFromMultifile(
-                p, fileToIndex[fc->Attribute("file")], data, ssize);
-            free(data);
-
-            if (!lsid.has_value())
+            if (fc->Attribute("name"))
             {
-                SCLOG("Wierd - no lsid value");
-                return false;
+                group->name = name + fc->Attribute("name");
             }
+            addedGroupIndices.push_back(groupId);
 
-            auto kr{90}, ks{0}, ke{127}, vs{0}, ve{127};
-            auto key = fc->FirstChildElement("key");
-            auto vel = fc->FirstChildElement("velocity");
-            if (key)
+            auto smp = fc->FirstChildElement("sample");
+            while (smp)
             {
-                key->QueryIntAttribute("root", &kr);
-                key->QueryIntAttribute("low", &ks);
-                key->QueryIntAttribute("high", &ke);
+                addSampleFromElement(smp, groupId);
+                smp = smp->NextSiblingElement("sample");
             }
-            if (vel)
-            {
-                vel->QueryIntAttribute("low", &vs);
-                vel->QueryIntAttribute("high", &ve);
-            }
-
-            auto group{0};
-            fc->QueryIntAttribute("group", &group);
-
-            if (group < 0 || group > addedGroupIndices.size())
-            {
-                SCLOG("Bad group : " << group);
-                return false;
-            }
-
-            auto &g = part->getGroup(addedGroupIndices[group]);
-            auto z = std::make_unique<engine::Zone>(*lsid);
-            z->mapping.rootKey = kr;
-            z->mapping.keyboardRange.keyStart = ks;
-            z->mapping.keyboardRange.keyEnd = ke;
-            z->mapping.velocityRange.velStart = vs;
-            z->mapping.velocityRange.velEnd = ve;
-            z->attachToSample(*engine.getSampleManager());
-            g->addZone(z);
         }
         else
         {
-            // SCLOG("Unhandled multisample field " << eln);
+            SCLOG("Ignored multisample field " << eln);
         }
         fc = fc->NextSiblingElement();
     }
 
     mz_zip_reader_end(&zip_archive);
-    return true;
+
+    if (!addedGroupIndices.empty())
+    {
+        engine.getSelectionManager()->selectAction(
+            selection::SelectionManager::SelectActionContents(pt, addedGroupIndices.front(), 0,
+                                                              true, true, true));
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 } // namespace scxt::multisample_support
