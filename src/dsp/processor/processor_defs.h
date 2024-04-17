f542713ee69d3d7cbae470c484d900831fcda9e6
Paul Walker <paul@pwjw.com>
2024-04-17 08:43:15 -0400

    Add a simple ring mod, like the SC2 one
    just with keytrack options!

--- src/dsp/processor/processor_defs.h
+++ src/dsp/processor/processor_defs.h
@@ -64,6 +64,7 @@
 #include "sst/voice-effects/distortion/Microgate.h"
 #include "sst/voice-effects/distortion/BitCrusher.h"
 #include "sst/voice-effects/distortion/Slewer.h"
+#include "sst/voice-effects/distortion/RingModulator.h"
 #include "sst/voice-effects/waveshaper/WaveShaper.h"
 
 #include "sst/voice-effects/eq/EqNBandParametric.h"
@@ -102,6 +103,9 @@ DEFINE_PROC(WaveShaper, sst::voice_effects::waveshaper::WaveShaper<SCXTVFXConfig
 DEFINE_PROC(Slewer, sst::voice_effects::distortion::Slewer<SCXTVFXConfig<1>>,
             sst::voice_effects::distortion::Slewer<SCXTVFXConfig<2>>, proct_fx_slewer, "Slewer",
             "Distortion", "slewer-fx");
+DEFINE_PROC(RingMod, sst::voice_effects::distortion::RingModulator<SCXTVFXConfig<1>>,
+            sst::voice_effects::distortion::RingModulator<SCXTVFXConfig<2>>, proct_fx_ringmod,
+            "Ring Modulator", "Distortion", "ringmod-fx");
 
 // Macros and commas don't get along
 namespace procimpl::detail
