8f3b451e4891e600317c8ab5f0e2d013e753e9fc
Paul Walker <paul@pwjw.com>
2024-05-30 18:55:04 -0400

    Implement the Serial2 routing shape

--- src/dsp/processor/routing.h
+++ src/dsp/processor/routing.h
@@ -28,70 +28,152 @@
 #ifndef SCXT_SRC_DSP_PROCESSOR_ROUTING_H
 #define SCXT_SRC_DSP_PROCESSOR_ROUTING_H
 
+#include "sst/basic-blocks/mechanics/block-ops.h"
+
 #include "processor.h"
 
 namespace scxt::dsp::processor
 {
-template <bool OS, int N, typename Mix, typename Endpoints>
-void processSequential(float fpitch, Processor *processors[engine::processorCount],
-                       bool processorConsumesMono[engine::processorCount], Mix &mix,
-                       Endpoints *endpoints, bool &chainIsMono, float output[2][N])
+
+template <bool OS, bool forceStereo, int N, typename Mix, typename Endpoints>
+inline void runSingleProcessor(int i, float fpitch, Processor *processors[engine::processorCount],
+                               bool processorConsumesMono[engine::processorCount], Mix &mix,
+                               Endpoints *endpoints, bool &chainIsMono, float input[2][N],
+                               float output[2][N])
 {
     namespace mech = sst::basic_blocks::mechanics;
 
-    float tempbuf alignas(16)[2][BLOCK_SIZE << 1];
+    float tempbuf alignas(16)[2][N];
+
+    endpoints->processorTarget[i].snapValues();
+
+    mix[i].set_target(*endpoints->processorTarget[i].mixP);
+
+    if constexpr (forceStereo)
+    {
+        processors[i]->process_stereo(input[0], input[1], tempbuf[0], tempbuf[1], fpitch);
+        mix[i].fade_blocks(input[0], tempbuf[0], output[0]);
+        mix[i].fade_blocks(input[1], tempbuf[1], output[1]);
+    }
+    else
+    {
+        if (chainIsMono && processorConsumesMono[i] &&
+            !processors[i]->monoInputCreatesStereoOutput())
+        {
+            // mono to mono
+            processors[i]->process_monoToMono(input[0], tempbuf[0], fpitch);
+            mix[i].fade_blocks(input[0], tempbuf[0], input[0]);
+        }
+        else if (chainIsMono && processorConsumesMono[i])
+        {
+            assert(processors[i]->monoInputCreatesStereoOutput());
+            // mono to stereo. process then toggle
+            processors[i]->process_monoToStereo(input[0], tempbuf[0], tempbuf[1], fpitch);
+
+            // this out[0] is NOT a typo. Input is mono
+            // And this order matters. Have to splat [1] first to keep [0] around
+            mix[i].fade_blocks(input[0], tempbuf[1], output[1]);
+            mix[i].fade_blocks(input[0], tempbuf[0], output[0]);
+
+            chainIsMono = false;
+        }
+        else if (chainIsMono)
+        {
+            assert(!processorConsumesMono[i]);
+            // stereo to stereo. copy L to R then process
+            mech::copy_from_to<blockSize << (OS ? 1 : 0)>(input[0], input[1]);
+            chainIsMono = false;
+            processors[i]->process_stereo(input[0], input[1], tempbuf[0], tempbuf[1], fpitch);
+            mix[i].fade_blocks(input[0], tempbuf[0], output[0]);
+            mix[i].fade_blocks(input[1], tempbuf[1], output[1]);
+        }
+        else
+        {
+            // stereo to stereo. process
+            processors[i]->process_stereo(input[0], input[1], tempbuf[0], tempbuf[1], fpitch);
+            mix[i].fade_blocks(input[0], tempbuf[0], output[0]);
+            mix[i].fade_blocks(input[1], tempbuf[1], output[1]);
+        }
+    }
+    // TODO: What was the filter_modout? Seems SC2 never finished it
+    /*
+    filter_modout[0] = voice_filter[0]->modulation_output;
+                           */
+}
+
+// -> 1 -> 2 -> 3 -> 4 ->
+template <bool OS, bool forceStereo, int N, typename Mix, typename Endpoints>
+inline void processSequential(float fpitch, Processor *processors[engine::processorCount],
+                              bool processorConsumesMono[engine::processorCount], Mix &mix,
+                              Endpoints *endpoints, bool &chainIsMono, float output[2][N])
+{
+    namespace mech = sst::basic_blocks::mechanics;
 
     for (auto i = 0; i < engine::processorCount; ++i)
     {
         if (processors[i])
         {
-            endpoints->processorTarget[i].snapValues();
-            // TODO: Snap int values here (and same in group)
-
-            mix[i].set_target(*endpoints->processorTarget[i].mixP);
-
-            if (chainIsMono && processorConsumesMono[i] &&
-                !processors[i]->monoInputCreatesStereoOutput())
-            {
-                // mono to mono
-                processors[i]->process_monoToMono(output[0], tempbuf[0], fpitch);
-                mix[i].fade_blocks(output[0], tempbuf[0], output[0]);
-            }
-            else if (chainIsMono && processorConsumesMono[i])
-            {
-                assert(processors[i]->monoInputCreatesStereoOutput());
-                // mono to stereo. process then toggle
-                processors[i]->process_monoToStereo(output[0], tempbuf[0], tempbuf[1], fpitch);
-
-                // this out[0] is NOT a typo. Input is mono
-                // And this order matters. Have to splat [1] first to keep [0] around
-                mix[i].fade_blocks(output[0], tempbuf[1], output[1]);
-                mix[i].fade_blocks(output[0], tempbuf[0], output[0]);
-
-                chainIsMono = false;
-            }
-            else if (chainIsMono)
-            {
-                assert(!processorConsumesMono[i]);
-                // stereo to stereo. copy L to R then process
-                mech::copy_from_to<blockSize << (OS ? 1 : 0)>(output[0], output[1]);
-                chainIsMono = false;
-                processors[i]->process_stereo(output[0], output[1], tempbuf[0], tempbuf[1], fpitch);
-                mix[i].fade_blocks(output[0], tempbuf[0], output[0]);
-                mix[i].fade_blocks(output[1], tempbuf[1], output[1]);
-            }
-            else
-            {
-                // stereo to stereo. process
-                processors[i]->process_stereo(output[0], output[1], tempbuf[0], tempbuf[1], fpitch);
-                mix[i].fade_blocks(output[0], tempbuf[0], output[0]);
-                mix[i].fade_blocks(output[1], tempbuf[1], output[1]);
-            }
-            // TODO: What was the filter_modout? Seems SC2 never finished it
-            /*
-            filter_modout[0] = voice_filter[0]->modulation_output;
-                                   */
+            runSingleProcessor<OS, forceStereo>(i, fpitch, processors, processorConsumesMono, mix,
+                                                endpoints, chainIsMono, output, output);
         }
+    }
+}
+
+// -> { 1 | 2 } -> { 3 | 4 } ->
+template <bool OS, bool forceStereo, int N, typename Mix, typename Endpoints>
+void processSerial2Pattern(float fpitch, Processor *processors[engine::processorCount],
+                           bool processorConsumesMono[engine::processorCount], Mix &mix,
+                           Endpoints *endpoints, bool &chainIsMono, float output[2][N])
+{
+    namespace mech = sst::basic_blocks::mechanics;
+
+    float tempbuf12 alignas(16)[2][BLOCK_SIZE << 1];
+    float tempbuf34 alignas(16)[2][BLOCK_SIZE << 1];
+
+    bool mono12 = chainIsMono;
+    bool mono34 = chainIsMono;
+
+    mech::copy_from_to<scxt::blockSize << OS>(output[0], tempbuf12[0]);
+    mech::copy_from_to<scxt::blockSize << OS>(output[1], tempbuf12[1]);
+
+    if (processors[0])
+    {
+        runSingleProcessor<OS, forceStereo>(0, fpitch, processors, processorConsumesMono, mix,
+                                            endpoints, mono12, tempbuf12, tempbuf12);
+    }
+    if (processors[1])
+    {
+        runSingleProcessor<OS, forceStereo>(1, fpitch, processors, processorConsumesMono, mix,
+                                            endpoints, mono12, tempbuf12, tempbuf12);
+    }
+
+    mech::copy_from_to<scxt::blockSize << OS>(output[0], tempbuf34[0]);
+    mech::copy_from_to<scxt::blockSize << OS>(output[1], tempbuf34[1]);
+
+    if (processors[2])
+    {
+        runSingleProcessor<OS, forceStereo>(2, fpitch, processors, processorConsumesMono, mix,
+                                            endpoints, mono34, tempbuf34, tempbuf34);
+    }
+    if (processors[3])
+    {
+        runSingleProcessor<OS, forceStereo>(3, fpitch, processors, processorConsumesMono, mix,
+                                            endpoints, mono34, tempbuf34, tempbuf34);
+    }
+
+    if constexpr (!forceStereo)
+    {
+        if (mono12 && mono34)
+        {
+        }
+    }
+    else
+    {
+        mech::accumulate_from_to<scxt::blockSize << OS>(tempbuf12[0], tempbuf34[0]);
+        mech::accumulate_from_to<scxt::blockSize << OS>(tempbuf12[1], tempbuf34[1]);
+
+        mech::copy_from_to<scxt::blockSize << OS>(tempbuf34[0], output[0]);
+        mech::copy_from_to<scxt::blockSize << OS>(tempbuf34[1], output[1]);
     }
 }
 } // namespace scxt::dsp::processor
