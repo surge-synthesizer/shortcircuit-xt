afb6148c33892aaa142a1bb0a851db2d9728c435
Paul <baconpaul@users.noreply.github.com>
2024-08-25 14:30:40 -0400

    Some directory cleanup (#1183)
    * Some directory cleanup
    
    1. doxygen config dir to docs
    2. remove the previous and unused code from git head since its
       in history and has already been absorbed into mainline
    
    * Cleanup the original now unused leak detector. Closes #1095

--- previous/tests/test_main.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_MAIN_H_
-#define TEST_MAIN_H_
-
-#include "catch2/catch2.hpp"
-#include "infrastructure/logfile.h"
-
-extern scxt::log::Level gTestLevel;
-extern scxt::log::LoggingCallback *gLogger;
-#endif
