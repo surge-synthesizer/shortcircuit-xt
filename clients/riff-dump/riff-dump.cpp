22ef3309be42700157103df1f9051c6bb2e8bf61
Paul Walker <paul@pwjw.com>
2025-04-20 07:50:37 -0400

    Add a small riff-dump utility I needed this morning

---
+++ clients/riff-dump/riff-dump.cpp
@@ -0,0 +1,93 @@
+/*
+ * Shortcircuit XT - a Surge Synth Team product
+ *
+ * A fully featured creative sampler, available as a standalone
+ * and plugin for multiple platforms.
+ *
+ * Copyright 2019 - 2024, Various authors, as described in the github
+ * transaction log.
+ *
+ * ShortcircuitXT is released under the Gnu General Public Licence
+ * V3 or later (GPL-3.0-or-later). The license is found in the file
+ * "LICENSE" in the root of this repository or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Individual sections of code which comprises ShortcircuitXT in this
+ * repository may also be used under an MIT license. Please see the
+ * section  "Licensing" in "README.md" for details.
+ *
+ * ShortcircuitXT is inspired by, and shares code with, the
+ * commercial product Shortcircuit 1 and 2, released by VemberTech
+ * in the mid 2000s. The code for Shortcircuit 2 was opensourced in
+ * 2020 at the outset of this project.
+ *
+ * All source for ShortcircuitXT is available at
+ * https://github.com/surge-synthesizer/shortcircuit-xt
+ */
+
+#include <iostream>
+#include <memory>
+#include "sst/plugininfra/version_information.h"
+
+#include "utils.h"
+
+#include "sample/exs_support/exs_import.h"
+
+#include "RIFF.h"
+
+
+void dumpList(RIFF::List *l, const std::string &pfx = "+-- ") {
+    SCLOG(pfx << "LIST: " << l->GetListTypeString() << " " << l->CountSubLists() << " " << l->CountSubChunks());
+    if (l->CountSubLists())
+    {
+        auto sl = l->GetFirstSubList();
+        while (sl)
+        {
+            dumpList(sl, "  " + pfx);
+            sl = l->GetNextSubList();
+        }
+    }
+    if (l->CountSubChunks())
+    {
+        auto sc = l->GetFirstSubChunk();
+
+        while (sc)
+        {
+            if (dynamic_cast<RIFF::List *>(sc))
+            {
+                dumpList(dynamic_cast<RIFF::List *>(sc), "  " + pfx);
+            }
+            else
+            {
+                SCLOG("  " << pfx << "CHUNK : " << sc->GetChunkIDString() << " " << sc->GetSize());
+            }
+            sc = l->GetNextSubChunk();
+        }
+    }
+};
+
+int main(int argc, char **argv)
+{
+    SCLOG("RIFF-DUMP");
+    SCLOG("    Version   = " << sst::plugininfra::VersionInformation::git_implied_display_version
+                             << " / "
+                             << sst::plugininfra::VersionInformation::project_version_and_hash);
+    if (argc < 2)
+    {
+        SCLOG("Usage: " << argv[0] << " <exs-file>");
+        return 1;
+    }
+    SCLOG("Opening " << argv[1]);
+
+    try
+    {
+        auto rf = std::make_unique<RIFF::File>(argv[1]);
+
+        dumpList(rf.get());
+    }
+    catch (const RIFF::Exception &e)
+    {
+        SCLOG("Exception: " << e.Message);
+    }
+    return 0;
+}
