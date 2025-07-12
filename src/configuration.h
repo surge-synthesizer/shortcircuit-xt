surge-synthesizer/shortcircuit-xt

commit d7ca5de53c19c802ef2dda1cf250150f7c40aa1e

Add Pitch, Amplitude and Tracking per variant
Does what it says on the tin really.

Closes #973

src/json/engine_traits.h
@@ -524,7 +524,8 @@ SC_STREAMDEF(scxt::engine::Zone::SingleVariant, SC_FROM({
                           {"loopFade", s.loopFade},
                           {"normalizationAmplitude", s.normalizationAmplitude},
                           {"pitchOffset", s.pitchOffset},
-                          {"amplitude", s.amplitude}};
+                          {"amplitude", s.amplitude},
+                          {"tracking", s.tracking}};
                  }
                  else
                  {
@@ -555,6 +556,7 @@ SC_STREAMDEF(scxt::engine::Zone::SingleVariant, SC_FROM({
                      findOrSet(v, "normalizationAmplitude", 0.f, s.normalizationAmplitude);
                      findOrSet(v, "pitchOffset", 0.f, s.pitchOffset);
                      findOrSet(v, "amplitude", 0.f, s.amplitude);
+                     findOrSet(v, "tracking", 1.f, s.tracking);
                  }
                  else
                  {

src-ui/app/edit-screen/components/mapping-pane/VariantDisplay.cpp
@@ -191,6 +191,24 @@ void VariantDisplay::rebuildForSelectedVariation(size_t sel, bool rebuildTabs)
         sampleAttachments[c] = std::move(at);
     };
 
+    auto attachFloatSamplePoint = [this](Ctrl c, const std::string &aLabel, auto &v) {
+        auto pmd = scxt::datamodel::describeValue(variantView.variants[selectedVariation], v);
+
+        auto at = std::make_unique<floatAttachment_t>(
+            pmd, [this](const auto &) { onSamplePointChangedFromGUI(); }, v);
+        auto sl = std::make_unique<jcmp::DraggableTextEditableValue>();
+        sl->setSource(at.get());
+        if (sampleEditors[c])
+        {
+            removeChildComponent(sampleEditors[c].get());
+            sampleEditors[c].reset();
+            sampleAttachments[c].reset();
+        }
+        addAndMakeVisible(*sl);
+        sampleEditors[c] = std::move(sl);
+        sampleFloatAttachments[c] = std::move(at);
+    };
+
     auto attachToDummy = [this](Ctrl c, const std::string &aLabel, uint32_t idx) {
         sampleEditors[c] = connectors::makeConnectedToDummy<jcmp::DraggableTextEditableValue>(
             idx, aLabel, 0.f, false, editor->makeComingSoon(aLabel));
@@ -263,13 +281,13 @@ void VariantDisplay::rebuildForSelectedVariation(size_t sel, bool rebuildTabs)
     addAndMakeVisible(*srcButton);
     addLabel(src, "SRC");
 
-    attachToDummy(volume, "Volume", 'vvol');
+    attachFloatSamplePoint(volume, "Volume", variantView.variants[selectedVariation].amplitude);
     addGlyph(volume, jcmp::GlyphPainter::GlyphType::VOLUME);
 
-    attachToDummy(trak, "Tracking", 'vtrk');
+    attachFloatSamplePoint(trak, "Tracking", variantView.variants[selectedVariation].tracking);
     addLabel(trak, "Trk");
 
-    attachToDummy(tune, "Tuning", 'vtun');
+    attachFloatSamplePoint(tune, "Tuning", variantView.variants[selectedVariation].pitchOffset);
     addGlyph(tune, jcmp::GlyphPainter::GlyphType::TUNING);
 
     if (loopActive)
@@ -583,7 +601,10 @@ void VariantDisplay::rebuild()
     {
         for (auto &[k, a] : sampleAttachments)
         {
-            a->sampleCount = samp->sample_length;
+            if (a)
+            {
+                a->sampleCount = samp->sample_length;
+            }
         }
 
         fileButton->setLabel(samp->displayName);

src-ui/app/edit-screen/components/mapping-pane/VariantDisplay.h
@@ -66,10 +66,14 @@ struct VariantDisplay : juce::Component, HasEditor
         tune
     };
 
+    using floatAttachment_t =
+        scxt::ui::connectors::PayloadDataAttachment<engine::Zone::SingleVariant>;
+
     std::unordered_map<Ctrl, std::unique_ptr<connectors::SamplePointDataAttachment>>
         sampleAttachments;
     std::unordered_map<Ctrl, std::unique_ptr<sst::jucegui::components::DraggableTextEditableValue>>
         sampleEditors;
+    std::unordered_map<Ctrl, std::unique_ptr<floatAttachment_t>> sampleFloatAttachments;
 
     std::unordered_map<Ctrl, std::unique_ptr<sst::jucegui::components::Label>> labels;
     std::unordered_map<Ctrl, std::unique_ptr<sst::jucegui::components::GlyphPainter>> glyphLabels;

src/datamodel/metadata_detail.h
@@ -44,6 +44,7 @@ template <typename P, typename V> ptrdiff_t offV(const P &p, const V &l)
 }
 template <typename P> sst::basic_blocks::params::ParamMetaData descFor(const P &, ptrdiff_t pd)
 {
+    // If you hit this assert you need an SC_DESCRIBE for a w.r.t b
     assert(false);
     return sst::basic_blocks::params::ParamMetaData()
         .withType(sst::basic_blocks::params::ParamMetaData::NONE)

src/engine/zone.h
@@ -123,7 +123,8 @@ struct Zone : MoveableOnly<Zone>, HasGroupZoneProcessors<Zone>, SampleRateSuppor
         float normalizationAmplitude{1.f}; // linear scale
         // per-sample pitch and amplitude
         float pitchOffset{0.f}; // semitones
-        float amplitude{0.f};   // db
+        float amplitude{1.f};   // cube to apply
+        float tracking{1.f};
 
         bool operator==(const SingleVariant &other) const
         {
@@ -279,6 +280,17 @@ SC_DESCRIBE(scxt::engine::Zone::ZoneOutputInfo,
                      pmd().asCubicDecibelAttenuationWithUpperDBBound(12).withName("Amplitude"));
             SC_FIELD(pan, pmd().asPan().withName("Pan"));
             SC_FIELD(procRouting, pmd().asInt().withRange(0, 1));)
+
+SC_DESCRIBE(scxt::engine::Zone::SingleVariant,
+            SC_FIELD(amplitude,
+                     pmd().asCubicDecibelAttenuationWithUpperDBBound(12).withName("Amplitude"));
+            SC_FIELD(pitchOffset, pmd().asSemitoneRange(-36, 36).withName("Pitch Offset}"));
+            SC_FIELD(tracking, pmd()
+                                   .asFloat()
+                                   .withRange(-2, 2)
+                                   .withName("Tracking")
+                                   .withLinearScaleFormatting("x")
+                                   .withDecimalPlaces(2));)
 
 SC_DESCRIBE(
     scxt::engine::Zone::ZoneMappingData, SC_FIELD(rootKey, pmd().asMIDINote().withName("Root Key"));

src/voice/voice.cpp
@@ -439,20 +439,19 @@ template <bool OS> bool Voice::processWithOS()
                 isGeneratorRunning[gidx] = !GD[gidx].isFinished;
                 isAnyGeneratorRunning = isAnyGeneratorRunning || isGeneratorRunning[gidx];
 
-                if (variantData.normalizationAmplitude != 1.0)
+                if (variantData.normalizationAmplitude != 1.0 || variantData.amplitude != 1.0)
                 {
+                    auto normBy = variantData.normalizationAmplitude * variantData.amplitude *
+                                  variantData.amplitude * variantData.amplitude;
                     auto *sl = (gidx == 0 ? output[0] : loutput[0]);
                     auto *sr = (gidx == 0 ? output[1] : loutput[1]);
                     if (monoGenerator[gidx])
                     {
-                        mech::scale_by<scxt::blockSize << (OS ? 1 : 0)>(
-                            variantData.normalizationAmplitude, sl);
-                        ;
+                        mech::scale_by<scxt::blockSize << (OS ? 1 : 0)>(normBy, sl);
                     }
                     else
                     {
-                        mech::scale_by<scxt::blockSize << (OS ? 1 : 0)>(
-                            variantData.normalizationAmplitude, sl, sr);
+                        mech::scale_by<scxt::blockSize << (OS ? 1 : 0)>(normBy, sl, sr);
                     }
                 }
 
@@ -956,8 +955,11 @@ void Voice::calculateGeneratorRatio(float pitch, int cSampleIndex, int generator
     GD.ratio = (int32_t)((1 << 24) * fac * zone->samplePointers[sampleIndex]->sample_rate * sampleRateInv *
                          (1.0 + modMatrix.getValue(modulation::vmd_Sample_Playback_Ratio, 0)));
 #endif
-    float ndiff =
-        pitch - zone->mapping.rootKey + zone->parentGroup->parentPart->configuration.tuning;
+    auto &var = zone->variantData.variants[sampleIndex];
+    auto kd = (pitch - zone->mapping.rootKey) * var.tracking;
+
+    float ndiff = kd + zone->parentGroup->parentPart->configuration.tuning + var.pitchOffset;
+
     auto fac = tuning::equalTuning.note_to_pitch(ndiff);
 
     GD[generatorIndex].ratio =

commit 16fcf3fbbb0ad9bc8ddb5eec488869dfd9589744

Fix a streaming nomralization regression
in  d7ca5de5 I introduced active normalization and amplitude
per variant but these values had been streamed with placeholder
zero. When I implemented I used amplitude not db as the in-engine
value. So I need to set them to 1, not zero, when unstreaming
old systems.

Closes #1703

src/configuration.h
@@ -33,7 +33,7 @@
 
 namespace scxt
 {
-static constexpr uint64_t currentStreamingVersion{0x2024'08'18};
+static constexpr uint64_t currentStreamingVersion{0x2025'06'19};
 
 static constexpr uint16_t blockSize{16};
 static constexpr uint16_t blockSizeQuad{16 >> 2};

src/json/engine_traits.h
@@ -563,9 +563,25 @@ SC_STREAMDEF(scxt::engine::Zone::SingleVariant, SC_FROM({
                      findOrSet(v, "interpolationType", dsp::InterpolationTypes::Sinc,
                                s.interpolationType);
                      findOrSet(v, "loopCountWhenCounted", 0, s.loopCountWhenCounted);
-                     findOrSet(v, "normalizationAmplitude", 0.f, s.normalizationAmplitude);
+
+                     findOrSet(v, "normalizationAmplitude", 1.f, s.normalizationAmplitude);
+                     findOrSet(v, "amplitude", 1.f, s.amplitude);
+                     if (SC_UNSTREAMING_FROM_PRIOR_TO(0x2025'06'19))
+                     {
+                         /*
+                          * These versions were streamed as zeros when unimplemented.
+                          * When I implemented them I implemented them as amplitude not
+                          * db in the engine, so fix. But there was a week or two where
+                          * the old version had the correc stream so level check before
+                          * whacking.
+                          */
+                         if (s.normalizationAmplitude < 1e-3)
+                             s.normalizationAmplitude = 1;
+                         if (s.amplitude < 1e-3)
+                             s.amplitude = 1;
+                     }
+
                      findOrSet(v, "pitchOffset", 0.f, s.pitchOffset);
-                     findOrSet(v, "amplitude", 0.f, s.amplitude);
                      findOrSet(v, "pan", 0.f, s.pan);
                  }
                  else