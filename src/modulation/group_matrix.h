4969d29e1481fb22d868682f598cf2ea36dbf123
Paul <baconpaul@users.noreply.github.com>
2025-09-06 11:26:24 -0400

    Support the expanded mod matrix api (which SC doesn't use) (#1810)
    Mod Matrix can now either provide base or just provide mod output.
    SC choses the later but still has to choose@!

--- src/modulation/group_matrix.h
+++ src/modulation/group_matrix.h
@@ -65,6 +65,7 @@ struct GroupMatrixConfig
     static bool supportsLag(const SourceIdentifier &s) { return true; }
 
     static constexpr bool IsFixedMatrix{true};
+    static constexpr bool ProvidesNonZeroTargetBases{true};
     static constexpr size_t FixedMatrixSize{scxt::modMatrixRowsPerGroup};
     static constexpr bool providesTargetRanges{true};
 
