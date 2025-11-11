975bfb9ed4596a1dbf4d31c95a3d95ff410a235f
Paul <baconpaul@users.noreply.github.com>
2025-11-11 10:42:26 -0500

    Implement part and bus fx swap (#1911)
    Implement part fx swap on the backend. 
    Implement bus fx swap on the backend
    
    So now this closes #1716

--- src/scxt-plugin/app/mixer-screen/MixerScreen.cpp
+++ src/scxt-plugin/app/mixer-screen/MixerScreen.cpp
@@ -207,8 +207,6 @@ void MixerScreen::setAllBussesToUniqueOutput()
 void MixerScreen::swapEffects(int bus1, int slot1, int bus2, int slot2, bool swapVsMove)
 {
     namespace cmsg = scxt::messaging::client;
-    SCLOG("Swapping effects " << bus1 << "/" << slot1 << " to " << bus2 << "/" << slot2
-                              << (swapVsMove ? " Swap" : " Move"));
     sendToSerialization(cmsg::SwapBusFX({bus1, slot1, bus2, slot2, swapVsMove}));
 }
 
